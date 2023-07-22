e7c0ee78b29e8a4b367942b3a8ba4da665e1b768
DodoJBNU <dh723@jbnu.ac.kr>
2023-07-22 11:13:26 +0900

    chore: eslint 적용

--- src/services/api.js
+++ src/services/api.js
@@ -1,45 +1,45 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   timeout: 1000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
-instance.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
   if (token) {
-    config.headers['Authorization'] = token;
+    config.headers["Authorization"] = token;
   }
   return config;
 });
 
 instance.interceptors.response.use(
-  response => {
+  (response) => {
     return response;
   },
-  error => {}
+  (error) => {}
 );
 
-export const register = data => {
+export const register = (data) => {
   const { email, password, username } = data;
-  return instance.post('/join', {
+  return instance.post("/join", {
     email,
     password,
     username,
   });
 };
 
-export const login = data => {
+export const login = (data) => {
   const { email, password } = data;
-  return instance.post('/login', {
+  return instance.post("/login", {
     email,
     password,
   });
 };
 
 export const fetchProducts = (page = 0) => {
-  return instance.get('/products' + '?page=' + page);
+  return instance.get("/products" + "?page=" + page);
 };
