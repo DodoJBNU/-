90e5ae644901f0866108d35706681b4182e5b578
DodoJBNU <dh723@jbnu.ac.kr>
2023-07-26 23:00:13 +0900

    Feat: Complete 'InputGroup' component

--- src/components/molecules/InputGroup.jsx
+++ src/components/molecules/InputGroup.jsx
@@ -1,11 +1,30 @@
 import Input from "../atoms/Input";
 import Box from "../atoms/Box";
-import Label from "../atoms/Label";
+import * as S from "../../styles/molecules/InputGroup";
 
-const InputGroup = ({ id, type, value, onChange, className, placeholder, label, name }) => {
+const ERROR_MSG = {
+  required: "필수 정보입니다.",
+  inValidEmail: "이메일 형식으로 작성해주세요.",
+  inValidPassword: "8~20자 영문, 숫자, 특수문자가 포함되어야하고 공백이 포함될 수 없습니다.",
+  inValidPasswordConfirm: "비밀번호가 일치하지 않습니다.",
+};
+
+const InputGroup = ({
+  id,
+  type,
+  value,
+  onChange,
+  className,
+  placeholder,
+  label,
+  name,
+  onBlur,
+  errorData = { username: "", email: "", password: "", passwordConfirm: "" },
+  headerText,
+}) => {
   return (
-    <Box className={className}>
-      <Label htmlFor={id}>{label}</Label>
+    <S.Div>
+      <S.Span>{headerText}</S.Span>
       <Input
         id={id}
         type={type}
@@ -13,8 +32,13 @@ const InputGroup = ({ id, type, value, onChange, className, placeholder, label, 
         onChange={onChange}
         placeholder={placeholder}
         name={name}
+        onBlur={onBlur}
+        className={className}
       />
-    </Box>
+      {errorData[name] ? (
+        <S.Span style={{ color: "red" }}>{ERROR_MSG[errorData[name]]}</S.Span>
+      ) : null}
+    </S.Div>
   );
 };
 
