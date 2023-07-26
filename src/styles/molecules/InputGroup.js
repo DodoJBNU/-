45ff346af2a2b92e6c020425ac8c0cfc7a2a476f
DodoJBNU <dh723@jbnu.ac.kr>
2023-07-26 23:05:25 +0900

    Feat: 'InputGroup' styled-component

---
+++ src/styles/molecules/InputGroup.js
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const Span = styled.span`
+  margin-bottom: 0.5rem;
+`;
