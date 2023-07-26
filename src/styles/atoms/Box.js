bc4b096247a2767cc9128cd16b2befcac2b36564
DodoJBNU <dh723@jbnu.ac.kr>
2023-07-26 22:41:28 +0900

    Feat : Styled-Components about Box Components

---
+++ src/styles/atoms/Box.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 10px;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+`;
