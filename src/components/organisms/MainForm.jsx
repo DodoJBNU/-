340223e2bd457a276a7f5f02bc0e119bcc492d28
DodoJBNU <dh723@jbnu.ac.kr>
2023-07-26 23:02:19 +0900

    Init: Create 'MainForm' component

---
+++ src/components/organisms/MainForm.jsx
@@ -0,0 +1,11 @@
+import Header from "../molecules/Header";
+import Container from "../atoms/Container";
+const MainForm = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default MainForm;
