471c05bae63744895fe3d64829769b07c938072a
DodoJBNU <dh723@jbnu.ac.kr>
2023-07-29 22:05:53 +0900

    Feat: Add MainLayout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/*공통 레이아웃: GNB , Footer*/}
-          <Route path="/" element={<HomePage />}></Route>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />}></Route>
+          </Route>
           {/*단독 레이아웃 */}
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/signup" element={<RegisterPage />}></Route>
