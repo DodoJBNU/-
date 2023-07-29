fe43e6b7a2e84b57d61b9db97520be56be59373f
DodoJBNU <dh723@jbnu.ac.kr>
2023-07-29 12:24:23 +0900

    Style: Page name change to 'HomePage'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import MainPage from "./pages/MainPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />}></Route>
+          {/*공통 레이아웃: GNB , Footer*/}
+          <Route path="/" element={<HomePage />}></Route>
+          {/*단독 레이아웃 */}
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/signup" element={<RegisterPage />}></Route>
         </Routes>
