25f3d72993d88d555d8f47a7dd2a55d7f7749ba5
DodoJBNU <dh723@jbnu.ac.kr>
2023-07-31 16:28:28 +0900

    Feat: Add initialState to

--- src/store/slices/productSlice.js
+++ src/store/slices/productSlice.js
@@ -1,10 +1,12 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { fetchProducts } from "../../services/product";
+import _ from "lodash";
 
 const initialState = {
   products: [],
   loading: false, // 요청을 보냈을 때는 true, 아닌 경우 : 요청이 없었거나, 실패했거나, 성공했을 때, false
   error: null, //  error exist : {message, status}
+  isEnd: false,
 };
 
 const productSlice = createSlice({
@@ -15,8 +17,11 @@ const productSlice = createSlice({
       state.loading = true;
     });
     builder.addCase(getProducts.fulfilled, (state, action) => {
+      if (action.payload.response.length < 10) {
+        state.isEnd = true;
+      }
       state.loading = false;
-      state.products = action.payload.response; // action.payload : {success, response, error }
+      state.products = _.uniqBy([...state.products, ...action.payload.response], "id");
       state.error = action.payload.error;
     });
     builder.addCase(getProducts.rejected, (state, action) => {
